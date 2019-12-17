174997205d70a3ac01e66591a01a3a2b38f2240d
dentnz <karl.lurman@gmail.com>
2019-12-17 21:11:14 +1100

    msu_data stuff

--- msu.v
+++ msu.v
@@ -1,244 +1,224 @@
-`define debug
+`define DEBUG;
 
-module msu_audio(
-  input clk,
-  input reset,
+module MSUDATA(
+  input		clk,
+  input		sd_buff_wr,
+  input	 [31:0]	msu_data_addr,
+
+  `ifndef DEBUG
+  input         reset,
+  input         msu_dataseekfinished_in,
+  input         msu_data_seek,
+  input 	sd_ack_2,    			// sd_ack[2]
   
-  `ifndef debug
-  input [31:0] img_size,
-  input trig_play,
-  input repeat_out,
-  input trackmounting_falling,
-  input sd_buff_wr,
-  input [11:0] audio_fifo_usedw,
-  input sd_ack_1,
+  output	msu_data_busy,
   `endif
-  
-  // Used to tell sd card handling which LBA to jump to
-  output reg [20:0] sd_lba_1,
-  // Allows us to skip over samples when loop points are not on a sector boundary
-  output reg ignore_sd_buffer_out,
-  output reg audio_play,
-  output reg [8:0] word_count,
-  output reg sd_rd_1 // Needs to be wired to sd_rd[1] in consumer of this module
+
+  output [31:0] sd_lba_2,
+  output	sd_rd_2 = 0, 			// sd_rd[2] 
+  output	clk_count,
+  output	msu_data_fifo_busy = 0,
+  output 	msu_dataseekfinished = 1,
+  output        msu_data_fifo_usedw,
+  output	msu_data_debug
 );
+  reg [7:0] msu_data_debug = 0;
+  reg msu_dataseekfinished = 1;
+  reg msu_data_fifo_busy = 0;
+  reg msu_data_seek_out = 0;
+  reg msu_data_seek_old = 0;
+  reg msu_dataseekfinished_in_old = 0;
 
-  reg [20:0] current_frame = 21'd0;
-  reg [20:0] max_end_frame = 21'd2097151;
-  
-  `ifndef debug
-  reg [31:0] loop_index = 32'd0;
-  reg [8:0] sector_size_words = 9'd256;
+  reg [7:0] msu_data_debug_dataseeks = 0;
+
+  `ifndef DEBUG
+
+  parameter LBA_MAX = 32'd60;
+  parameter FIFO_MAX_W = 16'd16128;
   `endif
   
-  `ifdef debug
-  ///////////////////////////////////////////////
-  // Debug testing things... please ignore if you
-  // are not running this in 8bitworkshop
-  // Assumed we are playing
-  reg audio_play = 0;
-  reg trig_play = 1;
-  reg trackmounting_falling = 1;
-  // Assumed image size
-  reg [63:0] img_size = 64'd1024;
-  // force repeating when high
-  reg repeat_out = 1;
-  // loop point in samples (not minus the 8 byte header)
-  reg [31:0] loop_index = 32'd20;
-  reg sd_ack_1 = 0;
-  reg sd_buff_wr = 1;
-  reg [31:0] audio_fifo_usedw = 32'd0;
-  // Typically this is 512 bytes
-  reg [8:0] sector_size_words = 9'd64;
-  ///////////////////////////////////////////////
-  `endif
-  
-  // End frame handling
-  // Have to be careful we don't go to negative one here?
-  wire [20:0] end_frame = (img_size[20:0] >> 9) - 1;
-  wire [8:0] end_frame_byte_offset = img_size[8:0];
-  wire [8:0] end_frame_word_offset = end_frame_byte_offset / 2;
-  reg [7:0] partial_frame_state = 8'd0;
-  
-  // Loop handling
-  wire [20:0] loop_frame = loop_index[20:0] >> 9;
-  // 2 words are added to deal with the 8 byte header of the pcm file
-  wire [8:0] loop_frame_word_offset = loop_index[8:0] + 9'd2;
-  reg looping = 0;
-  
-  reg [7:0] mode = 8'd0;
-  reg [7:0] state = 8'd0;
+  `ifdef DEBUG
+  parameter LBA_MAX = 32'd60;
+  parameter FIFO_MAX_W = 16'd100;
+  reg [15:0] msu_data_fifo_usedw = 0;
 
+  // Debug
+  reg [15:0] clk_count = 0; 
+  reg msu_data_seek = 0;
+  reg reset = 0;
+  reg sd_ack_2 = 0;
+  reg msu_dataseekfinished_in = 0;
+  wire msu_data_busy = msu_data_fifo_busy || (msu_dataseekfinished != 1);
+  
+  // Simulate some req/ack and events at specific clock cycles
   always @(posedge clk) begin
-    // We have a trigger to play...
-    if (reset || trig_play) begin
-      // Stop any existing audio playback
-      current_frame <= 0;
-      state <= 0;
-      audio_play <= 0;
-      word_count <= 0;
-      sd_lba_1 <= 0;
-      partial_frame_state <= 0;
-      looping <= 0;
-      // Work out the audio playback mode
-      if (!repeat_out) begin
-        // Audio is not repeating and will stop
-        mode <= 8'd1;
-      end else begin
-        // Audio is repeating
-        mode <= 8'd2;
-      end
-      `ifdef debug
-      sd_ack_1 <= 1'b1;
-      trig_play <= 0;
-      `endif
-    end else begin
-      case (state)
-        0: begin
-          // Audio track has finished mounting
-          if (trackmounting_falling) begin
-            audio_play <= 1'b1;
-            // Advance the SD card LBA and read in the next sector
-            sd_lba_1 <= current_frame;
-            // Go! (request a sector from the HPS). 256 WORDS. 512 BYTES.
-            sd_rd_1 <= 1'b1;
-            state <= state + 1;
-            `ifdef debug
-            trackmounting_falling <= 0;
-            `endif
-          end
-        end
-        1: begin
-          // Wait for ACK
-          // sd_ack goes high at the start of a sector transfer (and during)
-          if (sd_ack_1) begin
-            sd_rd_1 <= 1'b0;
-            // Sanity check
-            word_count <= 0;
-            state <= state + 1;
-          end
-        end
-        2: begin
-          // Keep collecting words until we hit the buffer limit 
-          if (sd_ack_1 && sd_buff_wr) begin
-            word_count <= word_count + 1;
-            if (looping) begin
-              // We may need to deal with some remainder samples after the loop frame
-              if (word_count < loop_frame_word_offset) begin
-                ignore_sd_buffer_out <= 1;
-              end else begin
-                looping <= 0;
-                ignore_sd_buffer_out <= 0;
-              end
-            end
-          end
-          if (word_count == sector_size_words) begin
-            word_count <= 0;
-            `ifdef debug
-            sd_ack_1 <= 0;
-            `endif
-          end
-          if (partial_frame_state == 1 && word_count == end_frame_word_offset) begin
-            word_count <= 0;
-            partial_frame_state <= 2;
-            `ifdef debug
-            sd_ack_1 <= 0;
-            `endif
-          end
-          // Only add new frames if we haven't filled the buffer
-          if (!sd_ack_1 && audio_fifo_usedw < 1792) begin
-            state <= state + 1;
-          end
-        end
-        3: begin
-          // Check if we've reached end_frame yet
-          if ((current_frame < end_frame) && audio_play) begin
-            current_frame <= current_frame + 1;
-            // Fetch another sector
-            sd_lba_1 <= sd_lba_1 + 1;
-            sd_rd_1 <= 1'b1;
-            state <= 1;
-            `ifdef debug
-            sd_ack_1 <= 1'b1;
-            `endif
-          end else begin
-            state <= state + 1;
-          end
-        end
-        4: begin
-          // Final frame handling
-          if (audio_play && end_frame_byte_offset == 0) begin
-            // Handle a full frame
-            if (mode == 8'd1) begin
-              // Full final frame, stopped
-              audio_play <= 0;
-              state <= 0;
-              sd_lba_1 <= 0;
-              looping <= 0;
-            end else begin
-              // Full final frame, Looped
-              current_frame <= loop_frame;
-              sd_lba_1 <= 0;
-              sd_rd_1 <= 1'b1;
-              state <= 1;
-              looping <= 1;
-              `ifdef debug
-              sd_ack_1 <= 1'b1;
-              `endif
-            end
-          end else begin
-            case (partial_frame_state)
-              0: begin
-                // Move to the partial frame, which will be the last full frame + 1
-                current_frame <= current_frame + 1;
-                sd_lba_1 <= current_frame + 1;
-                partial_frame_state <= 8'd1;
-              end 
-              1: begin
-                // Keep reading bytes from the file for the partial frame
-                sd_rd_1 <= 1'b1;
-                state <= 1;
-                `ifdef debug
-                sd_ack_1 <= 1'b1;
-                `endif
-              end
-              2: begin
-                // We've reached the end of the partial frame now.. handle stopping/looping
-                if (mode == 8'd1) begin
-                  // Stopping
-                  audio_play <= 0;
-                  state <= 0;
-                  sd_lba_1 <= 0;
-                  partial_frame_state <= 0;
-                end else begin
-                  // Loop
-                  looping <= 1;
-                  if (loop_frame == 0) begin
-                    // Loop frame is zero, so just go back to 0
-                    partial_frame_state <= 0;
-                    current_frame <= 0;  
-                    sd_lba_1 <= 0;
-                  end else begin
-                    // Our loop point is a non-zero sample, go back to the loop frame first 
-                    current_frame <= loop_frame;
-                    sd_lba_1 <= loop_frame;
-                    // We will deal with loop frame word offsets above
-                  end
-                  word_count <= 0;
-                  audio_play <= 1;
-                  state <= 1;
-                  sd_rd_1 <= 1'b1;
-                  `ifdef debug
-                  sd_ack_1 <= 1'b1;
-                  `endif
-                end
-              end
-            endcase
-          end
-        end 
-        default:; // Do nothing but wait
-      endcase
-    end	
-  end // Ends clocked block
+    clk_count <= clk_count + 1;
+    if (clk_count == 0) begin
+      reset <= 1;
+    end
+    
+    if (clk_count == 5) begin
+      reset <= 0;
+    end
+    
+      
+    if (sd_rd_2 == 1) begin
+      sd_ack_2 <= 1;
+    end
+    
+    if (sd_rd_2 ==0) begin
+      sd_ack_2 <= 0;
+      msu_data_fifo_usedw <= msu_data_fifo_usedw + 1;
+    end
+    
+    // MSU has seeked
+    if (clk_count == 21) begin
+      msu_data_seek <= 1;
+    end
+    
+    if (clk_count == 22) begin
+      msu_data_seek <= 0;
+    end
+    
+    // Hps has finished seeking
+    if (clk_count == 30) begin
+      msu_dataseekfinished_in <= 1;
+    end
+    
+    if (clk_count % 10 == 0) begin
+      msu_data_fifo_usedw <= msu_data_fifo_usedw - 5;
+    end
+  end    
+       
+`endif
+  
+// MSU Data track reading state machine
+always @(posedge clk)
+  if (reset) begin
+    msu_data_debug_dataseeks <= 0;
+    // pause the state machine in state 4 on reset
+    msu_data_state <= 8'd4;
+    sd_lba_2 <= 0;
+    sd_rd_2 <= 0;
 
-endmodule
+    msu_data_addr_bit1_old <= 0;
+
+    msu_data_fifo_busy <= 0;
+    msu_dataseekfinished <= 1;
+    msu_dataseekfinished_in_old <= 0;
+    msu_data_debug <= 8'd0;
+    msu_data_seek_old <= 0;
+  end else begin
+    // falling edge stuff
+    msu_data_addr_bit1_old <= msu_data_addr[1];
+    msu_dataseekfinished_in_old <= msu_dataseekfinished_in;
+    msu_data_seek_old <= msu_data_seek;
+
+    // For HPS seek pulse
+    msu_data_seek_out <= 0;
+		
+    if (msu_dataseekfinished_in_old == 1 && msu_dataseekfinished_in == 0) begin
+      msu_data_debug <= 8'd255;
+      msu_dataseekfinished <= 1;
+    end
+
+    // Rising edge
+    if (msu_data_seek_old == 0 && msu_data_seek == 1) begin
+      msu_data_debug_dataseeks <= msu_data_debug_dataseeks + 1;
+      // Both our fifo and hps are seeking
+      msu_data_fifo_busy <= 1;
+      msu_dataseekfinished <= 0;
+      // Init sd, fifo, internal counters
+      sd_lba_2 <= 0;
+      sd_rd_2 <= 1'b0;
+      // Tell the hps to seek now, one pulse
+      msu_data_seek_out <= 1;
+
+      // Kick off the state machine
+      msu_data_state <= 0;
+    end	
+	
+    case (msu_data_state)
+      0: begin
+        sd_rd_2 <= 1'b1;
+        msu_data_state <= msu_data_state + 1;
+      end
+      1: if (sd_ack_2) begin
+        // Sector transfer has started. (Need to check sd_ack[2], so we know the data is for us.)
+        sd_rd_2 <= 1'b0;
+        msu_data_state <= msu_data_state + 1;
+      end
+	
+      2: begin	
+        // See if we have filled up our 32 sector (16kb) buffer yet BEFORE we say our seek is finished
+        if (!sd_ack_2 & sd_lba_2 >= LBA_MAX) begin
+          
+          // Let the MSU know the seek has finished, at least in terms of the fifo buffer, hps could still
+          // be seeking...
+          msu_data_fifo_busy <= 0;
+
+          msu_data_debug <= 8'd7;
+          
+          if (msu_data_fifo_usedw < FIFO_MAX_W) begin
+            // Buffer is not full, so just top up as normal
+            sd_lba_2 <= sd_lba_2 + 1;
+            msu_data_state <= 0;
+            msu_data_debug <= 8'd22;
+          end
+        end else if (!sd_ack_2) begin
+          msu_data_debug <= 8'd6;
+
+          if (msu_data_fifo_usedw < FIFO_MAX_W) begin
+            // Buffer is not full, so just top up as normal
+            sd_lba_2 <= sd_lba_2 + 1;
+            msu_data_state <= 0;
+            msu_data_debug <= 8'd23;
+          end
+        end
+      end
+
+      4: begin
+        // Initial 'Paused' state
+        msu_data_debug <= 8'd5;
+      end
+
+      default:;
+    endcase
+  end
+
+  wire [15:0] msu_data_fifo_dout;
+  wire msu_data_fifo_empty;
+
+  reg msu_data_addr_bit1_old = 0;
+  reg msu_dataseekfinished_out_old = 0;
+
+  /*
+  wire msu_data_req;
+  wire [15:0] msu_data_fifo_usedw;
+
+wire msu_data_fifo_rdreq = msu_data_req && (msu_data_addr_bit1_old != msu_data_addr[1]);
+  
+  
+  // Clear the FIFO, for only ONE clock pulse, else it will clear the first sector we transfer.
+  wire msu_data_fifo_clear = msu_data_seek || reset;
+
+  wire msu_data_fifo_full;
+  wire msu_data_fifo_wr = !msu_data_fifo_full && sd_ack_2 && sd_buff_wr;
+  
+  msu_data_fifo msu_data_fifo_inst (
+    .sclr(msu_data_fifo_clear),
+    .clock(clk_sys),
+    .wrreq(msu_data_fifo_wr),
+    .full(msu_data_fifo_full),
+    .usedw(msu_data_fifo_usedw),
+    .data(sd_buff_dout),
+    .rdreq(msu_data_fifo_rdreq),
+    .empty(msu_data_fifo_empty),
+    .q(msu_data_fifo_dout)	
+  );
+  */
+  
+  reg [7:0] msu_data_state = 8'd4;
+
+endmodule;
+    
